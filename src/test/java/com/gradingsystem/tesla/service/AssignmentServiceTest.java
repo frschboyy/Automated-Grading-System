e561bc3aea1d01f71dbabfd663a5316fc6e97fec
Francis <frchianu@icsabidjan.org>
2025-03-25 15:14:39 +0300

    more debugging 7

--- src/test/java/com/gradingsystem/tesla/service/AssignmentServiceTest.java
+++ src/test/java/com/gradingsystem/tesla/service/AssignmentServiceTest.java
@@ -22,6 +22,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.gradingsystem.tesla.model.Assignment;
 import com.gradingsystem.tesla.repository.AssignmentRepository;
